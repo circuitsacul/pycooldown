c5b096b666a885de30a259a264ea51b44e6087c1
Lunarmagpie <Bambolambo0@gmail.com>
2022-11-24 01:01:49 -0500

    add build.py

---
+++ build.py
@@ -0,0 +1,12 @@
+from mypyc.build import mypycify
+
+mypyc_paths = [
+    "pycooldown/__init__.py",
+    "pycooldown/fixed_mapping.py",
+    "pycooldown/flexible_mapping.py",
+    "pycooldown/sliding_window.py",
+]
+
+
+def build(setup_kwargs):
+    setup_kwargs["ext"] = mypycify(mypyc_paths)
